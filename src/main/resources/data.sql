5742256d7ed0763b1c860a648521ff7768562bf3
pranav <pranavsinghbora@gmail.com>
2025-10-13 19:08:31 +0530

    Patient sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO patient (name, dob, email, gender, blood_group) VALUES
+('aman', '2001-04-01', 'aman@gmail.com', 'Male', 'B_POSITIVE'),
+('vijay', '2002-11-14', 'vijay@gmail.com', 'Male', 'A_POSITIVE'),
+('sofia', '2003-08-24', 'sofia@gmail.com', 'Female', 'O_POSITIVE');
