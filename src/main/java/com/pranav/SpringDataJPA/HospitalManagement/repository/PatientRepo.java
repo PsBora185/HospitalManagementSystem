41171d8db6a90d0fcb6c8ad6623375a8239d1744
pranav <pranavsinghbora@gmail.com>
2025-10-13 18:56:17 +0530

    Repository For Patient

---
+++ src/main/java/com/pranav/SpringDataJPA/HospitalManagement/repository/PatientRepo.java
@@ -0,0 +1,11 @@
+package com.pranav.SpringDataJPA.HospitalManagement.repository;
+
+import com.pranav.SpringDataJPA.HospitalManagement.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PatientRepo extends JpaRepository<Patient, Long> {
+
+}
