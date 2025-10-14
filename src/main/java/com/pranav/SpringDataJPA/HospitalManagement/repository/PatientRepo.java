6ef3e935daa5b3f461fd23e2a560f1b9a21170a0
pranav <pranavsinghbora@gmail.com>
2025-10-14 18:00:15 +0530

    Added more Custom methods to find data

--- src/main/java/com/pranav/SpringDataJPA/HospitalManagement/repository/PatientRepo.java
+++ src/main/java/com/pranav/SpringDataJPA/HospitalManagement/repository/PatientRepo.java
@@ -1,7 +1,15 @@
 package com.pranav.SpringDataJPA.HospitalManagement.repository;
 
+import com.pranav.SpringDataJPA.HospitalManagement.dtos.BloodGroupCountResponseDto;
 import com.pranav.SpringDataJPA.HospitalManagement.entity.Patient;
+import com.pranav.SpringDataJPA.HospitalManagement.entity.type.BloodGroup;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,9 +18,30 @@ public interface PatientRepo extends JpaRepository<Patient, Long> {
 
     Patient findByName(String name);
 
-    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+    List<Patient> findByDobOrEmail(LocalDate dob, String email);
 
-    List<Patient> findByBirthDateBetween(LocalDate startDate, LocalDate endDate);
+    List<Patient> findByDobBetween(LocalDate startDate, LocalDate endDate);
 
+    // find by name that contains query , id in descending order
     List<Patient> findByNameContainingOrderByIdDesc(String query);
+
+    @Query("SELECT p FROM Patient p where p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroup bloodGroup);
+
+    @Query("select p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+
+    // gives all type of blood group in db and their row count cv
+    @Query("select new com/pranav/SpringDataJPA/HospitalManagement/dtos/BloodGroupCountResponseDto.java" +
+            "(p.bloodGroup," + " Count(p)) from Patient p group by p.bloodGroup")
+    List<BloodGroupCountResponseDto> countEachBloodGroupType();
+
+    @Query(value = "select * from patient", nativeQuery = true)
+    Page<Patient> findAllPatients(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
+    int updateNameWithId(@Param("name") String name, @Param("id") Long id);
+
 }
