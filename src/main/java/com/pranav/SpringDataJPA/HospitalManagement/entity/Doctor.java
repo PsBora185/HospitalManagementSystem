2fd5ef729998b23617ec14f909defff1fe2dea4e
pranav <pranavsinghbora@gmail.com>
2025-10-15 20:18:22 +0530

    Doctor entity

---
+++ src/main/java/com/pranav/SpringDataJPA/HospitalManagement/entity/Doctor.java
@@ -0,0 +1,28 @@
+package com.pranav.SpringDataJPA.HospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 100)
+    private String specialization;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+
+}
