bca6d86e62ff2a5358933e1365de7f414d855182
pranav <pranavsinghbora@gmail.com>
2025-10-17 21:51:00 +0530

    Services For Entities

---
+++ src/main/java/com/pranav/SpringDataJPA/HospitalManagement/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package com.pranav.SpringDataJPA.HospitalManagement.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentService {
+}
