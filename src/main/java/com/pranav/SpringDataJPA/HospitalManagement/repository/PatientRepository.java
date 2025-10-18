c3cef964c0953cb1655c841e2641963ce2f022f0
pranav <pranavsinghbora@gmail.com>
2025-10-18 15:56:57 +0530

    Added custom method

--- src/main/java/com/pranav/SpringDataJPA/HospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/pranav/SpringDataJPA/HospitalManagement/repository/PatientRepository.java
@@ -14,7 +14,7 @@ import org.springframework.data.repository.query.Param;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface PatientRepo extends JpaRepository<Patient, Long> {
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     Patient findByName(String name);
 
@@ -43,5 +43,9 @@ public interface PatientRepo extends JpaRepository<Patient, Long> {
     @Modifying
     @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
     int updateNameWithId(@Param("name") String name, @Param("id") Long id);
+
+    // @Query("SELECT p FROM Patient p LEFT JOIN FETCH p.appointments a LEFT JOIN FETCH a.doctor")
+    @Query("SELECT p FROM Patient p LEFT JOIN FETCH p.appointments")
+    List<Patient> findAllPatientsWithAppointments();
 
 }
